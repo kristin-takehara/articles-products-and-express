23e7281fc239eb94d9fe6ae0b08bc660f5c95e03
Kristin Takehara <KRISTINTAKEHARA@Kristins-MacBook-Pro.local>
2017-10-09 15:04:39 -1000

    server connecting to browser

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 //jshint esversion: 6
-
+console.log("Cray Cray!");
 const express = require('express');
 const methodOverride = require('method-override');
+const exphbs = require('express-handlebars');
 const app = express();
 
 
@@ -17,7 +18,7 @@ const PORT = process.env.PORT || 8080;
 app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
-  res.send('testing');
+  res.send('testing, 1, 2');
 });
 
 app.listen(PORT, () => {
