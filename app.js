57faf9f43dfb648b93ca4ceea4abe5ec542a8dd1
Kristin Takehara <KRISTINTAKEHARA@Kristins-MacBook-Pro.local>
2017-10-11 11:55:49 -1000

    million dollar {}

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //jshint esversion: 6
-console.log("You Cray Cray!");
+// console.log("You Cray Cray!");
 
 const express = require('express');
 const methodOverride = require('method-override');
@@ -12,23 +12,25 @@ const articlesRoute = require('./routes/articles');
 
 const app = express();
 
-app.engine('.hbs', exphbs({
+app.engine('.hbs', exphbs({ //this sets the engine
   defaultLayout: 'main',
   extname: '.hbs'
 }));
+app.set('view engine', '.hbs');
 
 app.use(bodyParser.urlencoded({ "extended" : true})); //ensures server handles incoming requests through express
+
 app.use(methodOverride('_method'));
+// action="/products/4?_method=PUT" method="POST" enctype="application/x-www-form-urlencoded"
 
-app.use('/products', productsRoute);
-
-
-app.set('view engine', '.hbs');
 
 app.get('/', (req, res) => {
   // res.send('Testing 1, 2, 3');
-  res.render('home', {  });//<<---- pick up here
+  res.render('home');//<<------ pick up here???
 });
+
+app.use('/products', productsRoute);
+app.use('/articles', articlesRoute);
 
 app.listen(PORT, () => {
   console.log("Server listening on port: " + PORT);
